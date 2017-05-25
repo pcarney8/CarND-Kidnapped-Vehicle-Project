d21f1331931ff7e4b85b65a1322a311f3ef5ed40
Ubuntu <ubuntu@ip-172-31-52-1.ec2.internal>
2017-05-25 04:04:09 +0000

    removed variable collisions, passes

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -18,7 +18,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	random_device device;
 	mt19937 gen(device());
-	num_particles = 2;
+	num_particles = 20;
 //	cout << "init particles" << endl;
 	// Add random Gaussian noise to each particle.
 	normal_distribution<double> dist_x(x, std[0]);
@@ -35,7 +35,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 
 		particles.push_back(particle);
 	}
-	cout << "particle: " << particles[0].x << ", " << particles[0].y << ", " << particles[0].theta << endl;
+//	cout << "particle: " << particles[0].x << ", " << particles[0].y << ", " << particles[0].theta << endl;
 	is_initialized = true;
 }
 
@@ -48,7 +48,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
     double std_x = std_pos[0];
     double std_y = std_pos[1];
     double std_theta = std_pos[2];
-  //  cout << "prediction" << endl;
+//    cout << "prediction" << endl;
     //Zero mean Gaussians
     random_device device;
     mt19937 gen(device());
@@ -77,7 +77,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
         particles[i].theta += dist_theta(gen);
     }
 
-	cout << "particle: " << particles[0].x << ", " << particles[0].y << ", " << particles[0].theta << endl;
+//	cout << "particle: " << particles[0].x << ", " << particles[0].y << ", " << particles[0].theta << endl;
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
@@ -90,11 +90,13 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
     double min_dist;
 
     weights.clear();
-
+//	cout << "update weights" << endl;
+    
     for(auto& particle : particles){
-	    particle.weight = 1.0;
+	particle.weight = 1.0;
         landmarks_in_range.clear();
         //filter out the closest landmarks
+//	cout << "about to open map" << endl;
         for (auto landmark : map_landmarks.landmark_list) {
             distance = dist(landmark.x_f, landmark.y_f, particle.x, particle.y);
             if (distance <= sensor_range) {
@@ -107,6 +109,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
         transformed_observations.clear();
         //transform observations
+//	cout << "about to transform obs" << endl;
         for (size_t i=0; i<observations.size(); ++i) {
             transformed_observation.x = particle.x + (observations[i].x * cos(particle.theta)) - (observations[i].y * sin(particle.theta));
             transformed_observation.y = particle.y + (observations[i].x * sin(particle.theta)) + (observations[i].y * cos(particle.theta));
@@ -115,20 +118,21 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
         distance = 0.0;
         //DataAssociation find closest landmark
-        for (auto& observation : transformed_observations) {
+//	cout << "about to data associate" << endl; 
+        for (auto& o : transformed_observations) {
             min_dist = 999999;
 
             for (size_t j=0; j<landmarks_in_range.size(); ++j) {
-                distance = dist(observation.x, observation.y, landmarks_in_range[j].x, landmarks_in_range[j].y);
+                distance = dist(o.x, o.y, landmarks_in_range[j].x, landmarks_in_range[j].y);
                 if (distance < min_dist) {
                     min_dist = distance;
-                    observation.id = j;
+                    o.id = j;
                 }
             }
         }
 
         //calculate the weight
-        for(auto& observation : observations){
+        for(auto& observation : transformed_observations){
             double mu_x = landmarks_in_range[observation.id].x;
             double mu_y = landmarks_in_range[observation.id].y;
 
@@ -137,8 +141,8 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
             double exp_x = pow(observation.x - mu_x, 2.0)/(2.0 * std_x * std_x);
             double exp_y = pow(observation.y - mu_y,  2.0)/(2.0 * std_y * std_y);
             double w = (1.0/(2.0 * M_PI * std_x * std_y))*exp(-1.0*(exp_x + exp_y));
-	        cout << "calculated w: " << w << endl;
-	        cout << "particle w: " << particle.weight << endl;
+	    //cout << "calculated w: " << w << endl;
+	    //cout << "particle w: " << particle.weight << endl;
             particle.weight = particle.weight * w;
         }
 
